06777289c052167a04c8e8fcf8d4964d13979c42
taku795 <takufamilyd4@gmail.com>
2024-12-01 08:33:03 +0900

    Revert "【ユーザー名の正規化】ビルド後"
    This reverts commit 76a6f927fc2c86e9333f10850c3a38eb2494ca4e.

---
+++ UsernameNormalization/UsernameNormalization/FormControler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsernameNormalization
+{
+    public partial class FormControler : Form
+    {
+        private Dictionary<string, Type> formMapp;
+
+        public FormControler()
+        {
+            formMapp = new Dictionary<string, Type>
+            {
+                { "G000", typeof(Login) },
+                { "G001", typeof(Home) },
+            };
+        }
+
+        public void ShowForm(string formCode)
+        {
+            if (formMapp.TryGetValue(formCode, out Type formType))
+            {
+                // 指定されたフォームをインスタンス化して表示
+                Form form = (Form)Activator.CreateInstance(formType);
+                form.Show();
+            }
+            else
+            {
+                MessageBox.Show("指定されたコードのフォームが見つかりません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
